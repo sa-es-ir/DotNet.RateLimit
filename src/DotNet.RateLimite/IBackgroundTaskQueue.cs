ad41a534ba4a5d2560346d77a32c725e1cce54a3
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-02-06 15:19:15 +0330

    add interfaces

---
+++ src/DotNet.RateLimite/IBackgroundTaskQueue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DotNet.RateLimit
+{
+    public interface IRateLimitBackgroundTaskQueue
+    {
+        void QueueBackgroundWorkItem(Func<CancellationToken, Task> workItem);
+
+        Task<Func<CancellationToken, Task>> DequeueAsync(
+            CancellationToken cancellationToken);
+    }
+}
---
+++ src/DotNet.RateLimite/IRateLimitService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace DotNet.RateLimit
+{
+    /// <summary>
+    /// rate limit service
+    /// </summary>
+    public interface IRateLimitService
+    {
+        /// <summary>
+        /// consider request can proceed or not based on key
+        /// </summary>
+        /// <param name="resourceKey"></param>
+        /// <param name="periodInSec"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        Task<bool> HasAccessAsync(string resourceKey, int periodInSec, int limit);
+    }
+}
