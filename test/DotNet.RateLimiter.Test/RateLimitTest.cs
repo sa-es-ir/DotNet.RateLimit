efa6dbe5cca2246a25b1781d5d7a00bbe824b562
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-02-28 00:47:27 +0330

    add unit test

---
+++ test/DotNet.RateLimiter.Test/RateLimitTest.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace DotNet.RateLimiter.Test;
+
+public class RateLimitTest : TestSetup
+{
+    [Fact]
+    public void FistTest()
+    {
+        TestMessage.Should().Be("Rate limit Exceeded");
+    }
+}
