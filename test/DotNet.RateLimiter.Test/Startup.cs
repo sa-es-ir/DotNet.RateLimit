37bc9794713704e1f7d3c027b779401ea418e9e4
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-03-05 01:29:15 +0330

    add startup to use DI

---
+++ test/DotNet.RateLimiter.Test/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace DotNet.RateLimiter.Test;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services, HostBuilderContext context)
+    {
+        services.AddRateLimitService(context.Configuration);
+    }
+
+    public void ConfigureHost(IHostBuilder hostBuilder) =>
+        hostBuilder.ConfigureAppConfiguration((_, builder) =>
+        {
+            builder.AddJsonFile("appsettings.json");
+        });
+
+}
