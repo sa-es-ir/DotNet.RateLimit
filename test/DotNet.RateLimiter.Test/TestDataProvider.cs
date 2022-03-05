69fcfc048ac68a52c54cfdffae3fb3d6857e8ca7
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-03-05 13:06:02 +0330

    add test data provider for member data

---
+++ test/DotNet.RateLimiter.Test/TestDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace DotNet.RateLimiter.Test;
+
+public class TestDataProvider
+{
+    public static IEnumerable<object[]> OkTestDataWithNoParams =>
+        new List<object[]>
+        {
+            new object[] { 1, 60, HttpStatusCode.OK }
+        };
+}
