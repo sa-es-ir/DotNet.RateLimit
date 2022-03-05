a7b60672e150472bff05dadb1f9538877d9f94cf
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-03-05 13:06:25 +0330

    add test initializer for create instances

---
+++ test/DotNet.RateLimiter.Test/TestInitializer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DotNet.RateLimiter.ActionFilters;
+using DotNet.RateLimiter.Interfaces;
+using DotNet.RateLimiter.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DotNet.RateLimiter.Test;
+
+public class TestInitializer
+{
+    private static readonly Random Random;
+
+    static TestInitializer()
+    {
+        Random = new Random();
+    }
+
+    public static ActionContext SetupActionContext(string ipHeaderName = "X-Forwarded-For",
+        string ip = "127.0.0.1",
+        string controllerName = "TestController",
+        string actionName = "TestAction")
+    {
+        var httpContext = new DefaultHttpContext();
+        httpContext.Request.Headers.TryAdd(ipHeaderName, ip);
+
+        return new ActionContext(httpContext,
+            new RouteData(),
+            new ActionDescriptor()
+            {
+                RouteValues = new Dictionary<string, string?>()
+                {
+                    {"Controller", controllerName},
+                    {"Action", actionName}
+                }
+            },
+            new ModelStateDictionary());
+
+
+    }
+
+    public static Task<ActionExecutedContext> ActionExecutionDelegateNext(ActionContext actionContext)
+    {
+        var ctx = new ActionExecutedContext(actionContext, new List<IFilterMetadata>(), null!);
+        return Task.FromResult(ctx);
+    }
+
+    public static RateLimitAttribute CreateRateLimitFilter(IServiceScope scopeFactory, int limit, int periodInSec, string? routeParams = null,
+        string? queryParams = null, RateLimitScope scope = RateLimitScope.Action)
+    {
+        return new RateLimitAttribute(
+            scopeFactory.ServiceProvider.GetRequiredService<ILogger<RateLimitAttribute>>(),
+            scopeFactory.ServiceProvider.GetRequiredService<IRateLimitService>(),
+            scopeFactory.ServiceProvider.GetRequiredService<IOptions<RateLimitOptions>>())
+        {
+            Limit = limit,
+            PeriodInSec = periodInSec,
+            QueryParams = queryParams,
+            RouteParams = routeParams,
+            Scope = scope
+        };
+    }
+
+    public static string GetRandomIpAddress()
+    {
+        return $"{Random.Next(1, 255)}.{Random.Next(0, 255)}.{Random.Next(0, 255)}.{Random.Next(0, 255)}";
+    }
+}
