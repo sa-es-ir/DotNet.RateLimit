573ea324fc4744890fb0e67a27df20c11ee754b1
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-03-05 23:26:52 +0330

    add redis rate limit test

--- test/DotNet.RateLimiter.Test/InMemoryRateLimitTest.cs
+++ test/DotNet.RateLimiter.Test/InMemoryRateLimitTest.cs
@@ -1,22 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using DotNet.RateLimiter.ActionFilters;
 using DotNet.RateLimiter.Interfaces;
 using DotNet.RateLimiter.Models;
-using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Abstractions;
-using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Xunit;
 using Xunit.DependencyInjection;
 
 namespace DotNet.RateLimiter.Test;
@@ -26,162 +19,5 @@ public class InMemoryRateLimitTest : BaseRateLimitTest
 {
     public InMemoryRateLimitTest(IServiceScopeFactory scopeFactory) : base(scopeFactory)
     {
-    }
-}
-
-public class BaseRateLimitTest
-{
-    private readonly IServiceScopeFactory _scopeFactory;
-
-    public BaseRateLimitTest(IServiceScopeFactory scopeFactory)
-    {
-        _scopeFactory = scopeFactory;
-    }
-
-    [Theory]
-    [MemberData(nameof(TestDataProvider.OkTestDataWithNoParams), MemberType = typeof(TestDataProvider))]
-    public async Task NormalRequest_Returns_Ok(int limit, int periodInSec, Dictionary<string, object?> actionArguments, HttpStatusCode expectedResult)
-    {
-        using var scope = _scopeFactory.CreateScope();
-        var rateLimitAction = TestInitializer.CreateRateLimitFilter(scope, limit, periodInSec);
-
-        var actionContext = TestInitializer.SetupActionContext(ip: TestInitializer.GetRandomIpAddress());
-
-        var actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), actionArguments, null!);
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        actionExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)expectedResult);
-    }
-
-    [Theory]
-    [MemberData(nameof(TestDataProvider.TooManyRequestTestDataWithNoParams), MemberType = typeof(TestDataProvider))]
-    public async Task ExtraRequest_Returns_TooManyRequest(int limit, int periodInSec, Dictionary<string, object?> actionArguments, HttpStatusCode expectedResult)
-    {
-        using var scope = _scopeFactory.CreateScope();
-        var rateLimitAction = TestInitializer.CreateRateLimitFilter(scope, limit, periodInSec);
-
-        var actionContext = TestInitializer.SetupActionContext(ip: TestInitializer.GetRandomIpAddress());
-
-        var actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), actionArguments, null!);
-
-        //fist is ok
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        //second request should be banned
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        actionExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)expectedResult);
-    }
-
-    [Theory]
-    [MemberData(nameof(TestDataProvider.OkTestDataWithNoParams), MemberType = typeof(TestDataProvider))]
-    public async Task ExtraRequest_DifferentIp_Returns_Ok(int limit, int periodInSec, Dictionary<string, object?> actionArguments, HttpStatusCode expectedResult)
-    {
-        using var scope = _scopeFactory.CreateScope();
-        var rateLimitAction = TestInitializer.CreateRateLimitFilter(scope, limit, periodInSec);
-
-        var actionContext = TestInitializer.SetupActionContext(ip: TestInitializer.GetRandomIpAddress());
-
-        var actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), actionArguments, null!);
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        actionExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)expectedResult);
-
-        actionContext = TestInitializer.SetupActionContext(ip: TestInitializer.GetRandomIpAddress());
-
-        actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), actionArguments, null!);
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        actionExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)expectedResult);
-    }
-
-    [Theory]
-    [MemberData(nameof(TestDataProvider.OkTestDataWithNoParams), MemberType = typeof(TestDataProvider))]
-    public async Task ExtraRequest_WhiteListIp_Returns_OK(int limit, int periodInSec, Dictionary<string, object?> actionArguments, HttpStatusCode expectedResult)
-    {
-        using var scope = _scopeFactory.CreateScope();
-        var rateLimitAction = TestInitializer.CreateRateLimitFilter(scope, limit, periodInSec);
-
-        var actionContext = TestInitializer.SetupActionContext(ip: "8.8.8.8");//same ip in white list ip in appsettings.json
-
-        var actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), actionArguments, null!);
-
-        //fist is ok
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        //second request should be banned but in case of white list ip it's ok
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        actionExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)expectedResult);
-    }
-
-    [Theory]
-    [MemberData(nameof(TestDataProvider.OkTestDataWithRouteParams), MemberType = typeof(TestDataProvider))]
-    public async Task NormalRequest_WithRouteParam_Returns_OK(int limit, int periodInSec, Dictionary<string, object?> actionArguments, HttpStatusCode expectedResult)
-    {
-        using var scope = _scopeFactory.CreateScope();
-        var rateLimitAction = TestInitializer.CreateRateLimitFilter(scope, limit, periodInSec, routeParams: string.Join(",", actionArguments.Select(x => x.Key)));
-
-        var actionContext = TestInitializer.SetupActionContext(ip: TestInitializer.GetRandomIpAddress(), routeParams: actionArguments);
-
-        var actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), actionArguments, null!);
-
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        actionExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)expectedResult);
-    }
-
-    [Theory]
-    [MemberData(nameof(TestDataProvider.TooManyRequestTestDataWithRouteParams), MemberType = typeof(TestDataProvider))]
-    public async Task ExtraRequest_WithRouteParam_Returns_TooManyRequest(int limit, int periodInSec, Dictionary<string, object?> actionArguments, HttpStatusCode expectedResult)
-    {
-        using var scope = _scopeFactory.CreateScope();
-        var rateLimitAction = TestInitializer.CreateRateLimitFilter(scope, limit, periodInSec, routeParams: string.Join(",", actionArguments.Select(x => x.Key)));
-
-        var actionContext = TestInitializer.SetupActionContext(ip: TestInitializer.GetRandomIpAddress(), routeParams: actionArguments);
-
-        var actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), actionArguments, null!);
-
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        actionExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)expectedResult);
-    }
-
-    [Theory]
-    [MemberData(nameof(TestDataProvider.OkTestDataWithRouteAndQueryParams), MemberType = typeof(TestDataProvider))]
-    public async Task NormalRequest_WithRouteParam_QueryParam_Returns_OK(int limit, int periodInSec, Dictionary<string, object?> actionArguments, Dictionary<string, object> queryParams, HttpStatusCode expectedResult)
-    {
-        using var scope = _scopeFactory.CreateScope();
-        var rateLimitAction = TestInitializer.CreateRateLimitFilter(scope, limit, periodInSec,
-            string.Join(",", actionArguments.Select(x => x.Key)),
-            string.Join(",", queryParams.Select(x => x.Key)));
-
-        var actionContext = TestInitializer.SetupActionContext(ip: TestInitializer.GetRandomIpAddress(), routeParams: actionArguments, queryParams: queryParams);
-
-        var actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), actionArguments, null!);
-
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        actionExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)expectedResult);
-    }
-
-    [Theory]
-    [MemberData(nameof(TestDataProvider.TooManyRequestTestDataWithRouteAndQueryParams), MemberType = typeof(TestDataProvider))]
-    public async Task ExtraRequest_WithRouteParam_QueryParam_Returns_TooManyRequest(int limit, int periodInSec, Dictionary<string, object?> actionArguments, Dictionary<string, object> queryParams, HttpStatusCode expectedResult)
-    {
-        using var scope = _scopeFactory.CreateScope();
-        var rateLimitAction = TestInitializer.CreateRateLimitFilter(scope, limit, periodInSec, routeParams: string.Join(",", actionArguments.Select(x => x.Key)));
-
-        var actionContext = TestInitializer.SetupActionContext(ip: TestInitializer.GetRandomIpAddress(), routeParams: actionArguments, queryParams: queryParams);
-
-        var actionExecutingContext = new ActionExecutingContext(actionContext, new List<IFilterMetadata>(), actionArguments, null!);
-
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        await rateLimitAction.OnActionExecutionAsync(actionExecutingContext, () => TestInitializer.ActionExecutionDelegateNext(actionContext));
-
-        actionExecutingContext.HttpContext.Response.StatusCode.Should().Be((int)expectedResult);
     }
 }
---
+++ test/DotNet.RateLimiter.Test/RedisRateLimitTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Xunit.DependencyInjection;
+
+namespace DotNet.RateLimiter.Test;
+
+[Startup(typeof(StartupRedis))]
+public class RedisRateLimitTest : BaseRateLimitTest
+{
+    public RedisRateLimitTest(IServiceScopeFactory scopeFactory) : base(scopeFactory)
+    {
+    }
+}
