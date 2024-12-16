2caf2dce10f7e9ac21d2c32e17c90efe992ddab7
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-12-16 18:47:43 +0000

    add redis test containers

--- test/DotNet.RateLimiter.Test/Startup.cs
+++ test/DotNet.RateLimiter.Test/Startup.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+using Testcontainers.Redis;
 
 namespace DotNet.RateLimiter.Test;
 
@@ -26,6 +26,11 @@ public class StartupRedis
 {
     public void ConfigureServices(IServiceCollection services, HostBuilderContext context)
     {
+        var redis = new RedisTestContainer();
+        redis.InitializeAsync().Wait();// runs once and ok to wait
+
+        context.Configuration["RateLimitOption:RedisConnection"] = redis.ConnectionString;
+
         services.AddRateLimitService(context.Configuration);
     }
 
@@ -35,4 +40,28 @@ public class StartupRedis
             builder.AddJsonFile("appsettings_redis.json");
         });
 
+}
+
+public class RedisTestContainer : IAsyncDisposable
+{
+    private readonly RedisContainer _redisContainer;
+
+    public string ConnectionString => _redisContainer.GetConnectionString();
+
+    public RedisTestContainer()
+    {
+        _redisContainer = new RedisBuilder()
+            .WithImage("redis:7.4")
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _redisContainer.StartAsync();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _redisContainer.StopAsync();
+    }
 }
